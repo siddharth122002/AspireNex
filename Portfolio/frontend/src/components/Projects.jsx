09a874ab3cb8126c3fa0a33c019d0efd86487b2d
Siddharth <siddharth122002@gmail.com>
2024-07-27 22:46:17 +0530

    keeper

--- Portfolio/frontend/src/components/Projects.jsx
+++ Portfolio/frontend/src/components/Projects.jsx
@@ -4,6 +4,7 @@ import book from "../assets/bookstore.png";
 import crypto from "../assets/crypto.png";
 import social from "../assets/ssss.png";
 import quiz from "../assets/Quizz.png";
+import key from "../assets/key.png";
 import { UserContext } from "../context/UserContext";
 import gsap from 'gsap';
 import { useGSAP } from '@gsap/react';
@@ -11,46 +12,46 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
 
 function Projects() {
-  const {dark} = useContext(UserContext);
-  
+  const { dark } = useContext(UserContext);
 
-  useGSAP(()=>{
+
+  useGSAP(() => {
     const mm = gsap.matchMedia();
     mm.add("(min-width: 768px)", () => {
-      gsap.from(".card",{
-          y:30,
-          duration:1,
-          opacity:0,
-          scale:0.2,
-          scrollTrigger:{
-              trigger:".card",
-              scroller:"body",
-              // markers:true,
-              start: 'top 70%', 
-              end: 'top 50%',
-              scrub:4,
-          
-          },
-      })
-      gsap.from(".projects",{
-        y:30,
-        duration:1,
-        opacity:0,
-        scale:0.2,
-        scrollTrigger:{
-            trigger:".projects",
-            scroller:"body",
-            // markers:true,
-            start: 'top 70%', 
-            end: 'top 50%',
-            scrub:4,
-        
+      gsap.from(".card", {
+        y: 30,
+        duration: 1,
+        opacity: 0,
+        scale: 0.2,
+        scrollTrigger: {
+          trigger: ".card",
+          scroller: "body",
+          // markers:true,
+          start: 'top 70%',
+          end: 'top 50%',
+          scrub: 4,
+
         },
+      })
+      gsap.from(".projects", {
+        y: 30,
+        duration: 1,
+        opacity: 0,
+        scale: 0.2,
+        scrollTrigger: {
+          trigger: ".projects",
+          scroller: "body",
+          // markers:true,
+          start: 'top 70%',
+          end: 'top 50%',
+          scrub: 4,
+
+        },
+      })
     })
   })
-  })
   return (
-    <div className={dark?"dark":"light"}>
+    <div className={dark ? "dark" : "light"}>
       <section id="/projects" className="py-20  dark:bg-slate-800">
         <div className="container mx-auto px-4">
           <div className="text-center">
@@ -58,7 +59,16 @@ function Projects() {
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 ">
               <div className="card">
                 <ProjectCard
-                  
+
+                  title="Keeper"
+                  image={key}
+                  description="A secure password manager that stores all your website names and passwords in one convenient place. Keeper ensures easy access and management of your credentials."
+                  link="https://key-manager-frontend.vercel.app/"
+                />
+              </div>
+              <div className="card">
+                <ProjectCard
+
                   title="SciSpot"
                   image={sci}
                   description="A MERN stack web app featuring secure user authentication and a user-friendly interface to showcase science articles with Cloudinary integration."
@@ -82,27 +92,27 @@ function Projects() {
                 />
               </div>
               <div className="card">
-              <ProjectCard
-                
-                title="PicShare"
-                image={social}
-                description="A web app for sharing photos, featuring functionalities for liking, commenting, and secure user login."
-                link="https://pic-share-frontend-pink.vercel.app/"
-              />
+                <ProjectCard
+
+                  title="PicShare"
+                  image={social}
+                  description="A web app for sharing photos, featuring functionalities for liking, commenting, and secure user login."
+                  link="https://pic-share-frontend-pink.vercel.app/"
+                />
               </div>
               <div className="card">
-              <ProjectCard
-                
-                title="QuizMaker"
-                image={quiz}
-                description="Built with the MERN stack that enables users to create, take, and score quizzes. Users can design quizzes, share them, and receive immediate score on their performance."
-                link="https://quiz-frontend-swart.vercel.app/"
-              />
+                <ProjectCard
+
+                  title="QuizMaker"
+                  image={quiz}
+                  description="Built with the MERN stack that enables users to create, take, and score quizzes. Users can design quizzes, share them, and receive immediate score on their performance."
+                  link="https://quiz-frontend-swart.vercel.app/"
+                />
               </div>
-              
-              
-              
-              
+
+
+
+
             </div>
           </div>
         </div>
